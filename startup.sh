133289663ea63f923f7a66feee53da92352c8359
Ubuntu <ubuntu@ip-10-0-0-12.ec2.internal>
2021-09-20 07:30:40 +0000

    ansible-file

---
+++ startup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt-get update
+apt-get upgrade
+service apache2 start
+systemctl enable apache2
+~                                                                                                                                             
+~                                                                                                                                             
+~                                                                                                                                             
+~                                  
